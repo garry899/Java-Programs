33cb718dda8a0494f248fa8889fce680248f07d9
garry899 <gurinder893@gmail.com>
2022-04-20 22:30:42 -0400

    Apr 20, 2022 10:30PM

--- ReadingUserInputChallenge/src/com/company/Main.java
+++ ReadingUserInputChallenge/src/com/company/Main.java
@@ -1,21 +1,28 @@
 package com.company;
 
-import javax.swing.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        int total =0;
         Scanner str = new Scanner(System.in);
-
-        for (int i=1;i<=10;i++){
-            System.out.println("Enter number "+i);
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Enter number " + i);
             boolean hasNextInt = str.hasNextInt();
-            if (hasNextInt){
-                System.out.println("Invalid Number");
+            if (hasNextInt) {
+                int num = str.nextInt();
+               total =total + num;
+               if (num<0){
+                   System.out.println("Invalid Number");
+                   System.exit(0);
+               }
+            } else {
+                System.out.println("Unable to parse");
+                System.exit(0);
             }
         }
+        System.out.println("Sum is " + total);
+        str.close();
+        }
     }
-}
