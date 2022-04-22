b0f1c53a8336a4d3b5de1afde5cfd28c1160c810
garry899 <gurinder893@gmail.com>
2022-04-21 23:11:18 -0400

    Apr 21, 2022 10:42PM updated

--- MinAndMaxInputChallenge/src/com/company/Main.java
+++ MinAndMaxInputChallenge/src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         int counter = 1;
         int min = 0; // 20
         int max = 0; // 20
@@ -24,6 +24,46 @@ public class Main {
         }
         System.out.println("Min number is " + min);
         System.out.println("Max number is " + max);
+        scanner.close();*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        boolean first = true;  // to stop printing 0 as min or max
+
+        while (true){
+
+            System.out.println("Enter number:");
+            boolean isAnInt = scanner.hasNextInt();
+
+
+            if (isAnInt){
+                int number = scanner.nextInt();
+
+
+                if (first){  // to stop printing 0 as min or max
+                    first = false;
+                    min = number;
+                    max = number;
+                }
+
+                if (number>max){
+                    max = number;
+                }
+
+                if (number<min){
+                    min = number;
+                }
+
+            }else {
+                break;   // enter any alphabet to stop the loop
+            }
+
+            scanner.nextLine();  // handle input
+        }
+
+        System.out.println("min= "+ min +", max= "+ max);
         scanner.close();
     }
 }
